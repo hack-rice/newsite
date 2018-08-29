1ca56ed95bc11c650fd4b07cece8fcf0a91f70b7
achalvs <avs@achal.me>
2018-08-28 23:53:43 -0500

    add config for next-optimized-images

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
 module.exports = {
   webpack: config => {
+    // Config for next-optimized-images
+    [optimizedImages, {}],
+
     // Fixes npm packages that depend on `fs` module
     config.node = {
       fs: 'empty'
